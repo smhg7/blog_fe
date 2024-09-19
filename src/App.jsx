4a90409fd2e21f6347ae6cbf33e5ce13b275cb65
Syed Muhammad Hamza Gilani <109893232+smhg7@users.noreply.github.com>
2024-09-19 21:50:05 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Box,Typography} from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AllBlogs from './pages/AllBlogs';
 import Blog from './pages/Blog';
 import Login from './pages/Login';
 import AddBlog from './pages/AddBlog';
-import ResponsiveAppBar from './components/NavBar';
+import Test from './pages/Test';
+
+
 
 
 function App() {
@@ -16,7 +18,7 @@ function App() {
             <nav>
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="/about">About</a></li>
                     <li><a href="/login">Login</a></li>
                 </ul>
             </nav>
@@ -28,8 +30,11 @@ function App() {
           <Route path="/Blog/:blog_id" element={<Blog />} />
           <Route path="/login" element={<Login />} />
           <Route path="/addblog" element={<AddBlog />} />
+          <Route path="/about" element={<Test />} />
         </Routes>
       </BrowserRouter>
+      
+      
     </>
   );
 }
